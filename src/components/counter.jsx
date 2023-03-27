76754eac88cc18c4350888c0b1bdb0a78118c704
suhailphp <suhailphp@gmail.com>
2023-03-27 21:56:22 +0400

    Rendering list

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,17 +2,28 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 2,
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"],
   };
   render() {
     return (
       <>
-        <span style={{ fontSize: 20 }} className="badge bg-primary m-2">
+        <span style={{ fontSize: 20 }} className={this.getBadgeClass()}>
           {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map((tag) => {
+            return <li key={tag}>{tag}</li>;
+          })}
+        </ul>
       </>
     );
+  }
+  getBadgeClass() {
+    const { count } = this.state;
+    let className = "badge m-2 bg-";
+    return count === 0 ? className + "warning" : className + "primary";
   }
   formatCount() {
     const { count } = this.state;
