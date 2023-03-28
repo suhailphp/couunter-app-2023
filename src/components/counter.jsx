1b77c71b72cf521dac433bbc1e6344c33f376f8c
suhailphp <suhailphp@gmail.com>
2023-03-28 17:50:10 +0400

    Render list with no data message

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,25 +2,19 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    tags: ["sdds", "dfsdff", "sdfds"],
+    tags: ["tag1", "tag2", "tag2"],
   };
   render() {
-    return <>{this.renderList()}</>;
-  }
-
-  renderList() {
-    const { tags } = this.state;
-    if (tags.length === 0) {
-      return <h1>No tags</h1>;
-    } else {
-      return (
+    return (
+      <>
+        {this.state.tags.length === 0 && <p>There are no tags</p>}
         <ul>
           {this.state.tags.map((tag) => {
             return <li key={tag}>{tag}</li>;
           })}
         </ul>
-      );
-    }
+      </>
+    );
   }
 }
 
