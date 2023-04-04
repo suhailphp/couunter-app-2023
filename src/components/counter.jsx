a322ab54cba336520c857b59069ccfdb71fea3b1
suhailphp <suhailphp@gmail.com>
2023-04-04 15:27:47 +0400

    composing component passing props

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
   };
   render() {
     return (
-      <>
+      <div>
         <span style={{ fontSize: 20 }} className={this.getBadgeClass()}>
           {this.formatCount()}
         </span>
@@ -16,23 +16,23 @@ class Counter extends Component {
         >
           Increment
         </button>
-      </>
+      </div>
     );
   }
 
   handleIncrement = (product) => {
     console.log(product);
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   getBadgeClass() {
-    const { count } = this.state;
+    const { value } = this.state;
     let className = "badge m-2 bg-";
-    return count === 0 ? className + "warning" : className + "primary";
+    return value === 0 ? className + "warning" : className + "primary";
   }
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
---
+++ src/components/counters.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+class Counters extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 2 },
+      { id: 2, value: 0 },
+      { id: 3, value: 1 },
+      { id: 4, value: 5 },
+    ],
+  };
+  render() {
+    return (
+      <>
+        {this.state.counters.map((counter) => {
+          return <Counter value={counter.value} key={counter.id} />;
+        })}
+      </>
+    );
+  }
+}
+
+export default Counters;
