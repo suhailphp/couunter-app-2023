750db97f794a32ab219acf61df4d6c3c1518db8e
suhailphp <suhailphp@gmail.com>
2023-05-21 15:52:23 +0400

    Header component seperated

--- src/App.js
+++ src/App.js
@@ -2,24 +2,12 @@
 // import logo from './logo.svg';
 import './App.css';
 import Counters from "./components/counters";
+import Header from './components/header';
 
 function App() {
   return (
     <>
-      <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-      <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-        <svg className="bi me-2" width="40" height="32"><use ></use></svg>
-        <span className="fs-4">Simple header</span>
-      </a>
-
-      <ul className="nav nav-pills">
-        <li className="nav-item"><a href="/"  className="nav-link active" aria-current="page">Home</a></li>
-        <li className="nav-item"><a href="/"  className="nav-link">Features</a></li>
-        <li className="nav-item"><a href="/"  className="nav-link">Pricing</a></li>
-        <li className="nav-item"><a href="/"  className="nav-link">FAQs</a></li>
-        <li className="nav-item"><a href="/"  className="nav-link">About</a></li>
-      </ul>
-    </header>
+    <Header></Header>
     <main className="container">
         <Counters></Counters>
       </main>
