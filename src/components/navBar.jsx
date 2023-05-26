3a629b22bc62dc7c7224312e514319e92a84ccab
suhailphp <suhailphp@gmail.com>
2023-05-26 14:33:44 +0400

    Lifting the state up to app component

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-function NavBar(props) {
+const NavBar = (props) => {
   return ( 
         <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
           <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
@@ -18,5 +17,3 @@ function NavBar(props) {
      );
 }
 export default NavBar;
-
-
